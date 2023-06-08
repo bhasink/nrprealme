e833be7df2209c1a9640758f60a181043640ddeb
bhasink <bhasink69@gmail.com>
2023-06-08 15:11:50 +0530

    sdf

--- pages/photo.js
+++ pages/photo.js
@@ -1,42 +1,49 @@
-"use client"; 
-import { useState, useRef, createRef, useEffect} from 'react'
-import {Camera} from "react-camera-pro";
+'use client'
+import { useState, useRef, createRef, useEffect } from 'react'
+// import {Camera} from "react-camera-pro";
 import { useScreenshot, createFileName } from 'use-react-screenshot'
-import axios from "axios"
+import axios from 'axios'
 import Link from 'next/link'
-import Router , {useRouter}  from 'next/router';
-import { CirclesWithBar } from  'react-loader-spinner'
+import Router, { useRouter } from 'next/router'
+import { CirclesWithBar } from 'react-loader-spinner'
+import Camera from 'react-html5-camera-photo'
+import 'react-html5-camera-photo/build/css/index.css'
 
 export default function Photo() {
-
-  const camera = useRef(null);
-  const [image, setImage] = useState(null);
-  const [numberOfCameras, setNumberOfCameras] = useState(0);
+  const camera = useRef(null)
+  const [image, setImage] = useState(null)
+  const [numberOfCameras, setNumberOfCameras] = useState(0)
   const ref = createRef(null)
   const [imaget, takeScreenshot] = useScreenshot({
     quality: 1.0,
-});
+  })
   const router = useRouter()
+  const [dataUri, setDataUri] = useState('')
 
   useEffect(() => {
-   
-    if(imaget != null){
+    if (imaget != null) {
       saveImg(imaget)
     }
-  
   }, [imaget])
 
+  const handleTakePhotoAnimationDone = (dataUri) => {
+    console.log(dataUri)
+    setDataUri(dataUri)
+  }
 
-  const download = async(image, { name = "realme", extension = "jpg" } = {}) => {
-    const a = document.createElement("a");
-    a.href = image;
-    a.download = createFileName(extension, name);
-    a.click();
-  };
+  const download = async (
+    image,
+    { name = 'realme', extension = 'jpg' } = {},
+  ) => {
+    const a = document.createElement('a')
+    a.href = image
+    a.download = createFileName(extension, name)
+    a.click()
+  }
 
-  const getImage = () => takeScreenshot(ref.current);
-   
-  const saveImg = async(im) => {
+  const getImage = () => takeScreenshot(ref.current)
+
+  const saveImg = async (im) => {
     // takeScreenshot(ref.current)
 
     // window.localStorage.setItem("imaget", imaget);
@@ -44,40 +51,39 @@ export default function Photo() {
     let formData = new FormData()
     formData.append('image', imaget)
 
-  //   fetch('https://phpstack-709751-3121510.cloudwaysapps.com/api/realme', {
-  //     method: 'post',
-  //     headers: {'Content-Type':'application/json'},
-  //     body: formData
-  //  }).then(response => response.json())
-  //  .then(data => window.localStorage.setItem("imaget", data.data.img));
-
+    //   fetch('https://phpstack-709751-3121510.cloudwaysapps.com/api/realme', {
+    //     method: 'post',
+    //     headers: {'Content-Type':'application/json'},
+    //     body: formData
+    //  }).then(response => response.json())
+    //  .then(data => window.localStorage.setItem("imaget", data.data.img));
 
     try {
-      const { data } = await axios.post('https://phpstack-709751-3121510.cloudwaysapps.com/api/realme', formData)
-     
-      window.localStorage.setItem("imaget", data.data.img);
+      const { data } = await axios.post(
+        'https://phpstack-709751-3121510.cloudwaysapps.com/api/realme',
+        formData,
+      )
+
+      window.localStorage.setItem('imaget', data.data.img)
 
       router.push('/result')
-
 
       console.log(data)
     } catch (err) {
       console.log(err)
     }
-
   }
 
-  if(imaget){
-    window.localStorage.setItem("imagett", imaget);
+  if (imaget) {
+    window.localStorage.setItem('imagett', imaget)
   }
-
 
   return (
     <main>
+      <div className="rmfds">
+       
 
-      <div className='rmfds'>
-
-      {/* <div>
+        {/* <div>
       <div>
         <button style={{ marginBottom: '10px' }} onClick={getImage}>
           Take screenshot
@@ -92,11 +98,19 @@ export default function Photo() {
       </div>
     </div> */}
 
-
-        <div className='camdivst' ref={ref}>
-        <Camera ref={camera} facingMode={"environment"}  numberOfCamerasCallback={setNumberOfCameras} />
-        <img src={"/arbigsnws.png"} alt='filter' style={{position:"relative", zIndex:8}} />
-
+        <div className="camdivst" ref={ref}>
+          {/* <Camera ref={camera} facingMode={"environment"}  numberOfCamerasCallback={setNumberOfCameras} /> */}
+         
+          <Camera
+          idealResolution={{ width: 401, height: 652 }}
+          // onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
+          isFullscreen={false}
+        />
+          <img
+            src={'/arbigsnws.png'}
+            alt="filter"
+            style={{ position: 'relative', zIndex: 8 }}
+          />
         </div>
 
         {/* <div className='outssdv'>
@@ -104,40 +118,39 @@ export default function Photo() {
         
         </div> */}
 
-       
-       <div className='center-ctayl'>
+        <div className="center-ctayl">
+          {!imaget && (
+            <button
+              className="btn btn-register file-upload iconcm"
+              onClick={getImage}
+            >
+              <i class="fas fa-camera"></i>
+            </button>
+          )}
 
-       {!imaget && (
-        <button className='btn btn-register file-upload iconcm'  onClick={getImage}>
-        <i class="fas fa-camera"></i>
-        </button>
+          {imaget && (
+            // <Link
+            //   href={{
+            //     pathname: '/result',
+            //   }}
+            //   className='btn btn-register file-upload'>
+            //   Result
+            // </Link>
+            <CirclesWithBar
+              height="50"
+              width="50"
+              color="#ffc915"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+              outerCircleColor=""
+              innerCircleColor=""
+              barColor=""
+              ariaLabel="circles-with-bar-loading"
+            />
+          )}
 
-)}
-
-       {imaget && (
-        // <Link 
-        //   href={{
-        //     pathname: '/result',
-        //   }}
-        //   className='btn btn-register file-upload'>
-        //   Result
-        // </Link>
-        <CirclesWithBar
-  height="50"
-  width="50"
-  color="#ffc915"
-  wrapperStyle={{}}
-  wrapperClass=""
-  visible={true}
-  outerCircleColor=""
-  innerCircleColor=""
-  barColor=""
-  ariaLabel='circles-with-bar-loading'
-/>
-        )}
-
-
-{  /* <button className='btn btn-register file-upload iconcm bcmp'   
+          {/* <button className='btn btn-register file-upload iconcm bcmp'   
         
         disabled={numberOfCameras <= 1}
         onClick={() => {
@@ -151,18 +164,12 @@ export default function Photo() {
 
         <img src='./flipcmct.png'/>
       </button>  */}
-       
-       {/**<button className='btn btn-register file-upload' onClick={() => setImage(camera.current.takePhoto())}>Take photo</button> */} 
-        
-       
-      </div>
 
-      
+          {/**<button className='btn btn-register file-upload' onClick={() => setImage(camera.current.takePhoto())}>Take photo</button> */}
+        </div>
       </div>
 
       {/* <img src={image} alt='Taken photo' /> */}
-
-
     </main>
   )
 }
