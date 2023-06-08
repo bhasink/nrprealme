47e508e3a7afa8e6e779294aad2b22469345dd83
bhasink <bhasink69@gmail.com>
2023-06-08 10:08:35 +0530

    env dada

--- pages/photo.js
+++ pages/photo.js
@@ -14,7 +14,7 @@ export default function Photo() {
   const [numberOfCameras, setNumberOfCameras] = useState(0);
   const ref = createRef(null)
   const [imaget, takeScreenshot] = useScreenshot({
-    quality: 0.8,
+    quality: 0.7,
 });
   const router = useRouter()
 
