ca77b7093386f27bbd2da090279985353bc4f9de
Ajeet <jeetay.yadav284@gmail.com>
2023-06-08 21:27:53 +0530

    changes in reac css and all images

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import Head from "next/head";
 
-import Script from 'next/script'
+
 
 import { useRouter } from 'next/router'
 
@@ -31,19 +31,7 @@ function MyApp({ Component, pageProps }) {
 
             <link rel="stylesheet" type="text/css" href={`/style.css`} />
 
-            <Script strategy="afterInteractive">
-       { 
-       `
-       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-       })(window,document,'script','dataLayer','GTM-T7XL3QQ');
-       
-       
-     
-`}
-</Script>
+            
            
         </Head>
         
