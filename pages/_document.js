adbb05503ac3bbe66c71af82c18e234f2a7219ac
bhasink <bhasink69@gmail.com>
2023-06-08 21:50:43 +0530

    realme

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,6 @@
 import Document, { Html, Main, Head, NextScript } from 'next/document'
 import { useState,useEffect } from "react"
 
-
-
-import Script from 'next/script'
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -16,19 +12,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
      
-        <Script strategy="afterInteractive">
-       { 
-       `
-       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-       })(window,document,'script','dataLayer','GTM-T7XL3QQ');
        
-       
-     
-`}
-</Script>
 
 
 
